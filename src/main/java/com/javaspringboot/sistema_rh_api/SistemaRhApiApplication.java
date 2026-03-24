cb7e7fae411befc2f94da4379ca4b572a3c7f93d
j-adenilson <j.adenilson@outlook.com.br>
2026-03-23 21:32:44 -0300

    Criando Projeto e Repositório

---
+++ src/main/java/com/javaspringboot/sistema_rh_api/SistemaRhApiApplication.java
@@ -0,0 +1,13 @@
+package com.javaspringboot.sistema_rh_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaRhApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaRhApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaspringboot/sistema_rh_api/SistemaRhApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaspringboot.sistema_rh_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaRhApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
